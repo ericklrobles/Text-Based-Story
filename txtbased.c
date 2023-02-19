98f4f4dd25971a52d6e4593602076fcafb083612
ericklrobles <eglrobles@gmail.com>
2023-02-19 08:19:06 -0800

    commit2

--- txtbased.c
+++ txtbased.c
@@ -6,8 +6,9 @@
 
 int main(void)
 {
-   float input; 
+   float input, input2; 
    char input2;
+   
     printf("Choose your life 1.[a cartel drug lord] or 2.[a absolute chad of a father]\n");
     scanf("%f",&input);
     if (input == 1){
@@ -20,8 +21,15 @@ int main(void)
     }
     if (input == 2)
     {
-        printf("");
+        printf("You wake up and go wake up little ole jimmy\n");
+        printf("Do you tell him to go to the gym or go to school? (1/2)");
+        scanf("%f", &input2);
+        if (input2 == 1){
+            printf("You decide for him to skip school and go to the gym like an absolute chad\n");
+            printf("Its push day and your son only benches the bar what do you do?(1.give him roids/2.go get some milk)\n");
+        }
     }
+
     
 
     return 0;
